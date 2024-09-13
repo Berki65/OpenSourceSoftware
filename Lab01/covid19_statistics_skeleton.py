fe1951d7f07c46a80edde5042aec9410dd1513cf
berki <007berki@gmail.com>
2024-09-13 17:01:57 +0900

    ok

--- Lab01/covid19_statistics_skeleton.py
+++ Lab01/covid19_statistics_skeleton.py
@@ -1,32 +1,36 @@
 """
-Homework of Berkay Bentetik - 24170078
+Assignment from Berkay Bentetik - 24170078
 Python Lab 01 - COVID-19 Statistics
 """
+
 
 def normalize_data(n_cases, n_people, scale):
     # TODO) Calculate the number of cases per its population
     norm_cases = []
     for idx, n in enumerate(n_cases):
-        norm_cases.append((n / n_people[idx]) * scale)
+        norm_cases.append((n / n_people[idx]) * scale)  # Scale could be 1000000 but its better this way
     return norm_cases
 
-regions  = ['Seoul', 'Gyeongi', 'Busan', 'Gyeongnam', 'Incheon', 'Gyeongbuk', 'Daegu', 'Chungnam', 'Jeonnam', 'Jeonbuk', 'Chungbuk', 'Gangwon', 'Daejeon', 'Gwangju', 'Ulsan', 'Jeju', 'Sejong']
-n_people = [9550227,  13530519, 3359527,     3322373,   2938429,     2630254, 2393626,    2118183,   1838353,   1792476,    1597179,   1536270,   1454679,   1441970, 1124459, 675883,   365309] # 2021-08
-n_covid  = [    644,       529,      38,          29,       148,          28,      41,         62,        23,        27,         27,        33,        16,        40,      20,      5,        4] # 2021-09-21
 
-sum_people = sum(n_people) # TODO) The total number of people
-sum_covid  = sum(n_covid) # TODO) The total number of new cases
-norm_covid = normalize_data(n_covid, n_people, 1000000) # Normalize Data Function
+regions = ['Seoul', 'Gyeongi', 'Busan', 'Gyeongnam', 'Incheon', 'Gyeongbuk', 'Daegu', 'Chungnam', 'Jeonnam', 'Jeonbuk',
+           'Chungbuk', 'Gangwon', 'Daejeon', 'Gwangju', 'Ulsan', 'Jeju', 'Sejong']
+n_people = [9550227, 13530519, 3359527, 3322373, 2938429, 2630254, 2393626, 2118183, 1838353, 1792476, 1597179, 1536270,
+            1454679, 1441970, 1124459, 675883, 365309]  # 2021-08
+n_covid = [644, 529, 38, 29, 148, 28, 41, 62, 23, 27, 27, 33, 16, 40, 20, 5, 4]  # 2021-09-21
+
+sum_people = sum(n_people)  # TODO) The total number of people
+sum_covid = sum(n_covid)  # TODO) The total number of new cases
+norm_covid = normalize_data(n_covid, n_people, 1000000)  # Normalize Data Function
 
 # Print population by region
 print('### Korean Population by Region')
 print('* Total population:', sum_people)
-print() # Print an empty line
+print()  # Print an empty line
 print('| Region | Population | Ratio (%) |')
 print('| ------ | ---------- | --------- |')
 
 for idx, pop in enumerate(n_people):
-    ratio = (pop / sum_people) * 1000000 # TODO) The ratio of new cases to the total
+    ratio = (pop / sum_people) * 1000000  # TODO) The ratio of new cases to the total
     print('| %s | %d | %.1f |' % (regions[idx], pop, ratio))
 
 print()
@@ -40,5 +44,5 @@ print()
 print('| Region | New Cases | Ratio (%) | New Cases per / 1M |')
 
 for idx, n in enumerate(n_covid):
-    ratio = (n / sum_covid) * 100 #Ratio like above in population by region
+    ratio = (n / sum_covid) * 100  #Ratio like above in population by region
     print('| %s | %d | %.1f | %.1f |' % (regions[idx], n, ratio, norm_covid[idx]))
