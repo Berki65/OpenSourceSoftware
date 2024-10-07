d056d5f1ebbf1d2eb51b6cf9fc6a9956dbd941e2
berki <007berki@gmail.com>
2024-10-07 13:55:46 +0900

    almost

--- Lab04/class_score_plot_skeleton.py
+++ Lab04/class_score_plot_skeleton.py
@@ -16,11 +16,29 @@ if __name__ == '__main__':
     # TODO) Prepare midterm, final, and total scores
     midterm_kr, final_kr = zip(*class_kr)
     total_kr = [40/125*midterm + 60/100*final for (midterm, final) in class_kr]
-    midterm_en, final_en = [0, 0]
-    total_en = [0]
+    midterm_en, final_en = zip(*class_en)
+    total_en = [40/125*midterm + 60/100*final for (midterm, final) in class_en]
 
+    fig, ax = plt.subplots(1, 2, figsize=(15, 5))
     # TODO) Plot midterm/final scores as points
+    ax[0].scatter(midterm_kr, final_kr, color='red', label='Korean')
+    ax[0].scatter(midterm_en, final_en, color='blue', label='English')
+    ax[0].set_xlim(0, 125)
+    ax[0].set_ylim(0, 100)
+    ax[0].set_title('Midterm vs Final')
+    ax[0].set_xlabel('Midterm')
+    ax[0].set_ylabel('Final')
+    ax[0].grid(True)
+    ax[0].legend()
+
 
     # TODO) Plot total scores as a histogram
+    ax[1].hist(total_kr, bins=10, alpha=0.5, color='red', label='Korean')
+    ax[1].hist(total_en, bins=10, alpha=0.5, color='blue', label='English')
+    ax[1].legend()
+    ax[1].set_xlim(0, 100)
+    ax[1].set_title('Total Score')
+    ax[1].set_xlabel('Total')
+    ax[1].set_ylabel('Frequency')
 
     plt.show()
