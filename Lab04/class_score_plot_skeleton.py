e1c0332f3a5b0650e07eea9bd3e0b83cdb3bbb9c
berki <007berki@gmail.com>
2024-10-07 18:49:01 +0900

    ˚

--- Lab04/class_score_plot_skeleton.py
+++ Lab04/class_score_plot_skeleton.py
@@ -22,28 +22,34 @@ if __name__ == '__main__':
     midterm_kr, final_kr = zip(*class_kr)
     total_kr = [40/125*midterm + 60/100*final for (midterm, final) in class_kr]
     midterm_en, final_en = zip(*class_en)
-    total_en = [40/125*midterm + 60/100*final for (midterm, final) in class_en]
+    total_en = [40 / 125 * midterm + 60 / 100 * final for (midterm, final) in class_en]
 
-    fig, ax = plt.subplots(1, 2, figsize=(15, 5))
     # TODO) Plot midterm/final scores as points
-    ax[0].scatter(midterm_kr, final_kr, color='red', label='Korean')
-    ax[0].scatter(midterm_en, final_en, color='blue', label='English')
-    ax[0].set_xlim(0, 125)
-    ax[0].set_ylim(0, 100)
-    ax[0].set_title('Midterm vs Final')
-    ax[0].set_xlabel('Midterm')
-    ax[0].set_ylabel('Final')
-    ax[0].grid(True)
-    ax[0].legend()
+    plt.figure(figsize=(7, 6))
+    plt.scatter(midterm_kr, final_kr, color='red', label='Korean')
+    plt.scatter(midterm_en, final_en, color='blue', label='English', marker='+')
+    plt.xlim(0, 125)
+    plt.ylim(0, 100)
+    plt.title('Midterm vs Final Scatter')
+    plt.xlabel('Midterm scores')
+    plt.ylabel('Final scores')
+    plt.grid(True)
+    plt.legend()
+    plt.text(37, 109, 'Berkay Bentetik - 24170078', fontsize=12, color='black', alpha=1, ha='right')
+    plt.savefig('class_score_scatter.png')
+    plt.show()
 
 
     # TODO) Plot total scores as a histogram
-    ax[1].hist(total_kr, bins=10, alpha=0.5, color='red', label='Korean')
-    ax[1].hist(total_en, bins=10, alpha=0.5, color='blue', label='English')
-    ax[1].legend()
-    ax[1].set_xlim(0, 100)
-    ax[1].set_title('Total Score')
-    ax[1].set_xlabel('Total')
-    ax[1].set_ylabel('Frequency')
+    plt.figure(figsize=(7, 6))
+    plt.hist(total_kr, bins=10, alpha=0.5, color='red', label='Korean')
+    plt.hist(total_en, bins=10, alpha=0.5, color='blue', label='English')
+    plt.legend()
+    plt.xlim(0, 100)
+    plt.title('Total Score')
+    plt.xlabel('Total scores')
+    plt.ylabel('The number of students')
 
+    plt.text(30, 16, 'Berkay Bentetik - 24170078', fontsize=12, color='black', alpha=1, ha='right')
+    plt.savefig('class_score_hist.png')
     plt.show()
