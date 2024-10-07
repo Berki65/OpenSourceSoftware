03a6e5230b7dfb03cdd5973f45199e070c4952e1
berki <007berki@gmail.com>
2024-10-07 20:47:02 +0900

    final

--- Lab04/class_score_plot_skeleton.py
+++ Lab04/class_score_plot_skeleton.py
@@ -18,38 +18,62 @@ if __name__ == '__main__':
     class_kr = read_data('data/class_score_kr.csv')
     class_en = read_data('data/class_score_en.csv')
 
-    # TODO) Prepare midterm, final, and total scores
+    # Read both Korean and English class scores
     midterm_kr, final_kr = zip(*class_kr)
     total_kr = [40/125*midterm + 60/100*final for (midterm, final) in class_kr]
     midterm_en, final_en = zip(*class_en)
     total_en = [40 / 125 * midterm + 60 / 100 * final for (midterm, final) in class_en]
 
-    # TODO) Plot midterm/final scores as points
+    # --- Scatter Plot ---
+    # Create a figure and set up the plot
     plt.figure(figsize=(7, 6))
     plt.scatter(midterm_kr, final_kr, color='red', label='Korean')
     plt.scatter(midterm_en, final_en, color='blue', label='English', marker='+')
+
+    #Limit for x and y-axis
     plt.xlim(0, 125)
     plt.ylim(0, 100)
+
+    # Labeling
     plt.title('Midterm vs Final Scatter')
     plt.xlabel('Midterm scores')
     plt.ylabel('Final scores')
+
+    # Add grid
     plt.grid(True)
-    plt.legend()
-    plt.text(37, 109, 'Berkay Bentetik - 24170078', fontsize=12, color='black', alpha=1, ha='right')
+
+    # Add legend
+    plt.legend(loc= 'upper left')
+
+    # Add watermark text
+    plt.text(28, 107, 'Berkay Bentetik - 24170078', fontsize=10, color='black', alpha=1, ha='right')
+
+    # Save and show the plot
     plt.savefig('class_score_scatter.png')
     plt.show()
 
 
-    # TODO) Plot total scores as a histogram
+    # --- Histogram Plot ---
+    # Create a figure and set up the plot
+    bins = range(0, 105, 5)
     plt.figure(figsize=(7, 6))
-    plt.hist(total_kr, bins=10, alpha=0.5, color='red', label='Korean')
-    plt.hist(total_en, bins=10, alpha=0.5, color='blue', label='English')
-    plt.legend()
+    plt.hist(total_kr, bins=bins, alpha=1, color='red', label='Korean')
+    plt.hist(total_en, bins=bins, alpha=0.3, color='blue', label='English')
+
+    # Limit for x-axis
     plt.xlim(0, 100)
-    plt.title('Total Score')
+
+    # Labeling
+    plt.title('Midterm vs Final Histogram')
     plt.xlabel('Total scores')
     plt.ylabel('The number of students')
 
-    plt.text(30, 16, 'Berkay Bentetik - 24170078', fontsize=12, color='black', alpha=1, ha='right')
+    # Add legend
+    plt.legend(loc='upper left')
+
+    # Add watermark text
+    plt.text(22, 9, 'Berkay Bentetik - 24170078', fontsize=10, color='black', alpha=1, ha='right')
+
+    # Save and show the plot
     plt.savefig('class_score_hist.png')
     plt.show()
